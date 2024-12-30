120901eb8b70d034f7de04bac5f23c0f217c0052
ahmedwasim1070 <ahmedwasim1070@gmail.com>
2024-12-30 22:07:07 +0500

    Learning c++

---
+++ C-Learning/Lecture-01.cpp
@@ -0,0 +1,45 @@
+// First C++ program :
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout<<"Hello World!";
+//     return 0;
+// }
+// Or
+// #include <iostream>
+// int main()
+// {
+//     std::cout << "Hello World!";
+//     return 0;
+// }
+
+// Executable CMD for cpp code:
+// g++ filename.cpp -o outputfilename.cpp
+// ./outputfilename.exe
+
+// Variables Data Types
+// Primitive Data Types:
+// int (4 Bytes)
+// char (1 Byte)
+// float (4 Bytes)
+// bool (1 Byte)
+// double (8 Bytes) >=13!
+
+// Type Casting
+// From Small data type to big data type is straight forward
+// But from big to small we use it like that also know as EXPLICIT TYPE CASTING
+// double price =100.99;
+// int newPrice=(int)price;
+// where paranthesis int will be the small data type that you want to convert
+
+// Taking input
+// int age;
+// cin >> age;
+
+// Problem
+// cout << (5/2) ; //2 answer
+// The answer shold be 2.5 but it will be 2 because both the input is int
+// To fix this we use EXPLICIT Type Casting
+// cout << (5/float(2)); // 2.5 answer
+//
