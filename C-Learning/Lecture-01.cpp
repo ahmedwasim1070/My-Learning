7c4c1df8c5a51c032cfb8afdf9c43388e524f520
ahmedwasim1070 <ahmedwasim1070@gmail.com>
2025-01-06 19:36:21 +0500

    m

--- C-Learning/Lecture-01.cpp
+++ C-Learning/Lecture-01.cpp
@@ -238,3 +238,107 @@
 //     cout << maxSum << " ";
 //     return 0;
 // }
+
+// Pair Sum Problem Sorted array
+// Brute Force approch
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// vector<int> pairSum(vector<int> nums, int target)
+// {
+//     vector<int> ans;
+//     int n = nums.size();
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (nums[i] + nums[j] == target)
+//             {
+//                 ans.push_back(i);
+//                 ans.push_back(j);
+//                 return ans;
+//             }
+//         }
+//     }
+//     return ans;
+// }
+// int main()
+// {
+//     vector<int> nums = {2, 7, 11, 15};
+//     int target = 9;
+//     vector<int> ans = pairSum(nums, target);
+//     cout << ans[0] << "," << ans[1] << endl;
+//     return 0;
+// }
+// More Optimized way only on sorted array
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// vector<int> pairSum(vector<int> nums, int target)
+// {
+//     vector<int> ans;
+//     int i = 0, j = nums.size();
+//     while (i < j)
+//     {
+//         int sum = nums[i] + nums[j];
+//         if (sum > target)
+//         {
+//             j--;
+//         }
+//         else if (sum < target)
+//         {
+//             i++;
+//         }
+//         else
+//         {
+//             ans.push_back(i);
+//             ans.push_back(j);
+//             return ans;
+//         }
+//     }
+//     return ans;
+// }
+// int main()
+// {
+//     vector<int> nums = {2, 7, 11, 15};
+//     int target = 9;
+//     vector<int> ans = pairSum(nums, target);
+//     cout << ans[0] << "," << ans[1] << endl;
+//     return 0;
+// }
+
+// Majority Element Problem
+// Brute Force Technique
+#include <iostream>
+#include <vector>
+using namespace std;
+int majority(vector<int> nums)
+{
+    for (int val : nums)
+    {
+        int fact = 0;
+        for (int num : nums)
+        {
+            if (val == num)
+            {
+                fact++;
+            }
+            else
+            {
+                fact = 0;
+            }
+        }
+        if (fact == nums.size() - 1 / 2)
+        {
+            return fact;
+        }
+    }
+    return 0;
+}
+int main()
+{
+    vector<int> nums = {1, 1, 2, 2, 1};
+    int res = majority(nums);
+    cout << res << endl;
+    return 0;
+}
